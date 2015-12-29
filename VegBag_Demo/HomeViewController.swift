38cc533d6f4ae8b4c22b6616c1055c6fdf0d1c22
矢野史洋 <a2.mj.pop@gmail.com>
2015-12-29 22:23:43 +0900

    first_commit

--- VegBag_Demo/HomeViewController.swift
+++ VegBag_Demo/HomeViewController.swift
@@ -8,16 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.cyanColor()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
